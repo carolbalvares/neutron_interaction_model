c7be1c6b0e6105c028c68f8d0a70100ddf09e24e
Carolina Alvares <carol.batista.alvares@gmail.com>
2023-04-28 11:40:29 -0300

    main file with discretization of samples

---
+++ main.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+# generating random samples
+samples = 100000
+values = np.random.choice(samples, size=10)
+
+#give them keys
+keys = [1,2,3,4,5,6,7,8,9,10]
+
+#assigning 
+keys_values=[]
+total=0
+([keys[i],values[i]])
+    total += values[i]
+for i in (range(len(keys_values))):
+    print(keys_values[i])
+    
+print(total)
