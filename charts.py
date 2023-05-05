79097af6c430639cd5a82fc7ffadfaeac1bf8c64
Carolina Alvares <carol.batista.alvares@gmail.com>
2023-05-05 16:26:08 -0300

    data frame generated

--- charts.py
+++ charts.py
@@ -1,2 +1,12 @@
 from main import * 
 from probability import *
+import seaborn as sns
+
+#setting theme
+sns.set_theme(style="darkgrid")
+
+tips = sns.load_dataset("tips")
+g = sns.jointplot(x="total_bill", y="tip", data=tips,
+                  kind="reg", truncate=False,
+                  xlim=(0, 60), ylim=(0, 12),
+                  color="m", height=7)
--- probability.py
+++ probability.py
@@ -26,33 +26,34 @@ free_neutrons = [100000]
 index = []
 #setting vars values
 del r_probability[0:discr_input]
-index = range(10-discr_input)
+r_index = range(10-discr_input)
 #looping
-for e in index:
+for e in r_index:
     free_neutrons.append(free_neutrons[e] * r_probability[e])
 free_neutrons.remove(100000)
 #converting floar into int
 free_neutrons_right = ['%i' % elem for elem in free_neutrons]
-#print("neutrons amount for each interaction: ", free_neutrons_right)
+# print("neutrons amount for each interaction: ", free_neutrons_right)
+# print(r_index)
 
 
 ##third probability: from specific discretization for the 0 : discr_input
 l_probability = np.array([0.7, 0.1, 0.9, 0.1, 0.6, 0.7, 0.6, 1, 0.45, 0.9])
 free_neutrons = [100000]
-index = []
+l_index = []
 #setting vars values
-index = range(discr_input)
-l_probability = np.take(l_probability, index)
+l_index = range(discr_input)
+l_probability = np.take(l_probability, l_index)
 #reversing probabilty array
 l_reversed_prob = l_probability[::-1]
-#print(l_reversed_prob)
+print(l_index)
 #looping
-for e in index:
+for e in l_index:
     free_neutrons.append(free_neutrons[e] * l_reversed_prob[e])
 free_neutrons.remove(100000)
 #converting floar into int
 free_neutrons_left = ['%i' % elem for elem in free_neutrons]
-#print("neutrons amount for each interaction: ", free_neutrons_left)
+print("neutrons amount for each interaction: ", free_neutrons_left)
 
 
 
