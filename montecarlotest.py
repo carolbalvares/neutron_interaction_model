1389d0106ee72bcb498f27b38ea5458b22df0f1c
Carolina Alvares <carol.batista.alvares@gmail.com>
2023-05-12 08:29:13 -0300

    changes in montecarlotest

--- montecarlotest.py
+++ montecarlotest.py
@@ -67,28 +67,28 @@ n_btw_numbers = pct_df.loc[pct_df['pct_between_to_target'] == True]
 print(n_btw_numbers)
 
 
-# # Define a list to keep all the results from each simulation that we want to analyze
-# all_stats = []
+# Define a list to keep all the results from each simulation that we want to analyze
+all_stats = []
 
-# # Loop through many simulations
-# for i in range(num_simulations):
+# Loop through many simulations
+for i in range(num_simulations):
 
-#     # Choose random inputs for the sales targets and percent to target
-#     sales_target = np.random.choice(sales_target_values, num_reps, p=sales_target_prob)
-#     pct_to_target = np.random.normal(avg, std_dev, num_reps).round(2)
+    # Choose random inputs for the sales targets and percent to target
+    sales_target = np.random.choice(sales_target_values, num_reps, p=sales_target_prob)
+    pct_to_target = np.random.normal(avg, std_dev, num_reps).round(2)
 
-#     # Build the dataframe based on the inputs and number of reps
-#     df = pd.DataFrame(index=range(num_reps), data={'Pct_To_Target': pct_to_target,
-#                                                    'Sales_Target': sales_target})
+    # Build the dataframe based on the inputs and number of reps
+    df = pd.DataFrame(index=range(num_reps), data={'Pct_To_Target': pct_to_target,
+                                                   'Sales_Target': sales_target})
 
-#     # Back into the sales number using the percent to target rate
-#     df['Sales'] = df['Pct_To_Target'] * df['Sales_Target']
+    # Back into the sales number using the percent to target rate
+    df['Sales'] = df['Pct_To_Target'] * df['Sales_Target']
 
-#     # Determine the commissions rate and calculate it
-#     df['Commission_Rate'] = df['Pct_To_Target'].apply(calc_commission_rate)
-#     df['Commission_Amount'] = df['Commission_Rate'] * df['Sales']
+    # Determine the commissions rate and calculate it
+    df['Commission_Rate'] = df['Pct_To_Target'].apply(calc_commission_rate)
+    df['Commission_Amount'] = df['Commission_Rate'] * df['Sales']
 
-#     # We want to track sales,commission amounts and sales targets over all the simulations
-#     all_stats.append([df['Sales'].sum().round(0),
-#                       df['Commission_Amount'].sum().round(0),
-#                       df['Sales_Target'].sum().round(0)])
+    # We want to track sales,commission amounts and sales targets over all the simulations
+    all_stats.append([df['Sales'].sum().round(0),
+                      df['Commission_Amount'].sum().round(0),
+                      df['Sales_Target'].sum().round(0)])
