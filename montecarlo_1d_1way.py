93a4a65e21b24baa6147636af61d7b9a89648154
Carolina Alvares <carol.batista.alvares@gmail.com>
2023-05-19 15:53:03 -0300

    add class def n_neutrons_cross

--- montecarlo_1d_1way.py
+++ montecarlo_1d_1way.py
@@ -1,34 +1,35 @@
 import numpy as np
+import pandas as pd
 
 # creating class
 class One_d_discr: 
     def __init__(self, num_samples):
         self.num_samples = num_samples
+    def n_neutrons_cross(cls, num_samples):
+        cls.num_samples = num_samples
         samp_cross_array = []
+        array_num_samples = []
         r_prob = np.random.rand(1).round(3)
-        print(r_prob)
         r_samples_array = np.random.rand(num_samples).round(3)
-        print(r_samples_array)
         while num_samples !=  0:
             for i in range(num_samples):
                 if r_samples_array[i] >= r_prob:
                     samp_cross_array.append(r_samples_array[i])
                     num_samples = num_samples - 1
-                print(num_samples)
-                print(samp_cross_array)
+            array_num_samples.append(num_samples)
             if num_samples != 0:
                 r_prob = np.random.rand(1).round(3)
-                print(r_prob)
                 r_samples_array = np.random.rand(num_samples).round(3)
-                print(r_samples_array)
             else:
-                break
-       
-            
+                break  
+        return array_num_samples
+
+
 #calling class
 initial_num_samples = int(input('Choose a initial number of samples:   '))
 d_1d_1way = One_d_discr(initial_num_samples)
-print(d_1d_1way)
+print(d_1d_1way.n_neutrons_cross(initial_num_samples))
+
 
 
 
