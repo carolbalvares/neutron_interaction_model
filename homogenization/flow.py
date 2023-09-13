e295989ef4ec583323e9f1ff9c02960801775808
Carolina Alvares <carol.batista.alvares@gmail.com>
2023-09-13 10:27:35 -0300

    one 3d plotting is working, but dont know if the result is correct

--- homogenization/flow.py
+++ homogenization/flow.py
@@ -7,12 +7,24 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
+from mpl_toolkits import mplot3d
+
+
+
 # given data
 r_o = active_core_diameter/2
 z_o = general_height
 
 def pos(j_0):
     return [x for x in j_0 if x > 0] 
+
+
+def matrix_flow(flow, index):
+    matriz = []
+    for i in range(0, len(flow), index):
+        sublista = flow[i:i+index]
+        matriz.append(sublista)
+    return matriz
 
 auxiliar = 2.405/r_o
 radius_array = np.arange(-0.5*np.pi, 0.5*np.pi, 0.1)
@@ -26,18 +38,32 @@ e_r = 200.7
 aux = 3.63 * power / (tt_act_core_vol * e_r * macro_cs_fission)
 height_array = np.arange(-0.5*np.pi, 0.5*np.pi, 0.1)
 
+data = {}
 
 
-flow = [0.0]
-for h in height_array:
-    for j in j_positive:
-        auxx = aux * j*np.cos(np.pi*h/z_o)
-        flow = np.append(flow, auxx)
+flow = np.empty((len(radius_array), len(height_array)))
+
+for i, h in enumerate(height_array):
+    for j, r in enumerate(radius_array):
+        auxx = aux * j * np.cos(np.pi * h / z_o)
+        flow[i, j] = auxx
+        
+  
+max = np.max(flow)
+print(max)
 
 
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.contour3D(radius_array, height_array, flow, 50, cmap='binary')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z');
+ax.view_init(60, 35)
+plt.title('Flow in function of radius and height')
 
-plt.plot(flow)
 plt.show()
+
 
 # poltar funcao de bessel e de altura de 0 a pi/2
 # fcs total = abs + scattering
@@ -45,3 +71,4 @@ plt.show()
 
 # data
 # https://docs.scipy.org/doc/scipy-1.0.0/reference/generated/scipy.special.jn.html
+#https://jakevdp.github.io/PythonDataScienceHandbook/04.12-three-dimensional-plotting.html
