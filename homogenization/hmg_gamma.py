b4e7ccde407f02f9fc3cfedf8a9d5cbf43c80eb3
Carolina Alvares <carol.batista.alvares@gmail.com>
2023-09-14 11:13:48 -0300

    changing macro and micro parameters in gamma

--- homogenization/hmg_gamma.py
+++ homogenization/hmg_gamma.py
@@ -5,18 +5,18 @@ from parameters import *
 # core homogenization
 
 # given data
-micro_gamma_U235 = 101
+micro_gamma_U235 = 98.81
 micro_gamma_U238 = 2.73
-micro_ab_O = 20 * 10**(-5)
-macro_ab_Fe = 0.22
-macro_ab_H2O = 0.022
+micro_gamma_O = 190*10**(-3)
+macro_gamma_Fe = 2.813
+macro_abs_H2O = 0.022
 
 
 
 # parameters
 macro_gamma_U235 = n_U235 * micro_gamma_U235 * 10 ** (-24)
 macro_gamma_U238 = n_U238 * micro_gamma_U238 * 10 ** (-24)
-macro_gamma_O = n_O * micro_ab_O * 10 ** (-24)
+macro_gamma_O = n_O * micro_gamma_O * 10 ** (-24)
 # print("238",macro_gamma_U238)
 # print("235",macro_gamma_U235)
 # print("O",macro_gamma_O)
@@ -28,12 +28,13 @@ macro_cs_gamma_fuel = ((macro_gamma_U235 + macro_gamma_U238 + macro_gamma_O)*(tt
 
 
 macro_cs_gamma = (((macro_gamma_U235 + macro_gamma_U238 + macro_gamma_O)*(tt_vol_UO2)) +
-                       (macro_ab_H2O * tt_vol_H2O) + (macro_ab_Fe * tt_vol_Fe))/tt_act_core_vol
+                       (macro_abs_H2O * tt_vol_H2O) + (macro_gamma_Fe * tt_vol_Fe))/tt_act_core_vol
 # print("macro_cs_gamma", macro_cs_gamma)
 
 micro_cs_gamma = (((macro_gamma_U235 + macro_gamma_U238 + macro_gamma_O)*(tt_vol_UO2)) +
-                       (macro_ab_H2O * tt_vol_H2O) + (macro_ab_Fe * tt_vol_Fe))/(tt_act_core_vol * (n_UO2 + n_H2O + n_Fe))
+                       (macro_abs_H2O * tt_vol_H2O) + (macro_gamma_Fe * tt_vol_Fe))/(tt_act_core_vol * (n_UO2 + n_H2O + n_Fe))
 
+print(micro_cs_gamma)
 # print("micro_cs_gamma", micro_cs_gamma)
 # source:
 # 1- Nuclear Reactor Analysis - James Duderstadt, Louis Hamilton  (data)
