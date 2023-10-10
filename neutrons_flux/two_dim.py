ba3f4904f39648f906ad075d9caf4a08883731e2
Carolina Alvares <carol.batista.alvares@gmail.com>
2023-10-10 13:38:52 -0300

    printing but not working

--- neutrons_flux/two_dim.py
+++ neutrons_flux/two_dim.py
@@ -4,95 +4,140 @@ from homogenization.hmg_gamma import macro_cs_gamma_fuel, macro_gamma_Fe
 from homogenization.hmg_fission import macro_cs_fission
 import sys
 sys.path.append('../')
+# import matplotlib.pyplot as plt
+# import pandas as pd
+# import seaborn as sns
 
 
-# Adjust the path as needed
-# material chosen: UO2
-
-# mi = 2/(3*m)
-# free_trn_path = 1/(macro_cs_UO2_scattering*(1-mi))
-# dif_coef = free_trn_path/3
-# sigma_s = round(macro_cs_UO2_scattering, 3)
-
-
-class One_d_one_material:
-    def __init__(self, num_samples, discretizations, prob_abs, prob_scat):
+class Two_d_one_material:
+    def __init__(self, num_samples, distance_array, tt_cross_section):
         self.num_samples = num_samples
-        self.discretizations = discretizations
-        self.prob_abs = prob_abs
-        self.prob_scat = prob_scat
+        self.distance_array = distance_array
+        self.tt_cross_section = tt_cross_section
 
-    def n_neutrons_cross(self, num_samples, discretizations, prob_abs, prob_scat):
-        self.discretizations = discretizations
+    def n_neutrons_cross(self, num_samples, distance_array, tt_cross_section):
+        self.distance_array = distance_array
         self.num_samples = num_samples
-        self.prob_abs = prob_abs
-        self.prob_scat = prob_scat
+        self.tt_cross_section = tt_cross_section
+        cross_matrix = np.zeros((3, 3))
         # cant_cross_array = np.array([])
         cross_array = np.array([])
-        aux_array = np.array([])
-        cross_matrix = np.zeros((1, num_samples))
-        
-        for j in range(6):
-            r_samples_array = np.random.rand(num_samples).round(3)
-            print("samples array:   ", r_samples_array)
-            aux_array = r_samples_array
-            discretizations_aux = discretizations
-            for e in range(discretizations_aux):
-                cross_array = np.array([])
-                cross_amount = 0
-                cant_cross_amount = 0
-                cant_cross_amount = False
-                num_samples_aux = num_samples
-                for i in range(num_samples_aux):
-                    if cant_cross_amount != False:
-                        if prob_abs[j, e] > aux_array[i] or prob_scat[j, e] > aux_array[i]:
-                            cant_cross_amount += 1
-                            # cant_cross_array = np.append(
-                            #     cant_cross_array, aux_array[i])
-                            cross_array = np.append(
-                                cross_array, 0.000
-                            )
+        cross_amount_array = np.array([])
+        dist_to_collision = 0
+        r_samples_array = np.random.rand(num_samples).round(3)
+        print("samples array:   ", r_samples_array)
+        elements_per_subarray = len(r_samples_array) // 8
+        divided_array = r_samples_array.reshape((8, elements_per_subarray))
+        aux_array = divided_array
+        for r in range(2):
+            for c in range(2):
+                if r%2!=0 or c%2!=0:
+                    for e in range(len(distance_array)):
+                        cross_array = np.array([])
+                        cross_amount = 0
+                        cant_cross_amount = 0
+                        cant_cross_amount = False
+                        num_samples_aux = num_samples/6
+                        for i in range(6):
+                            if aux_array[i][0] != 1:
+                                dist_to_collision = -np.log(1 - aux_array[i][0]) / tt_cross_section
+                            else:
+                                while aux_array[i][0] == 1:
+                                    aux_array[i][0] = round(np.random.rand(), 3)
+                                dist_to_collision = -np.log(1 - aux_array[i][0]) / tt_cross_section
+                            print("dist to collision", dist_to_collision)
+                            # print("dist to collision", dist_to_collision)
+                            if cant_cross_amount != False:
+                                if dist_to_collision < distance_array[e] :
+                                    cant_cross_amount += 1
+                                    cross_array = np.append(
+                                        cross_array, 0.000
+                                    )
+                                    cross_matrix[r][c] = aux_array[i][0]
+                                    print("cross matrix", cross_matrix)
+                                else:
+                                    cross_amount += 1
+                                    cross_array = np.append(cross_array, aux_array[i][0])
+                                    cross_matrix[r][c] = aux_array[i][0]
+                                    print("cross matrix", cross_matrix)
+                            else:
+                                if dist_to_collision < distance_array[e] :
+                                    cant_cross_amount = 1
+                                    cross_amount = 0
+                                    cross_array = np.append(cross_array, 0.000)
+                                    cross_matrix[r][c] = aux_array[i][0]
+                                    print("cross matrix", cross_matrix)
+                                else:
+                                    cross_amount = 1
+                                    cross_array = np.append(cross_array, aux_array[i][0])
+                                    cant_cross_amount = 0
+                                    cross_matrix[r][c] = aux_array[i][0]
+                                    print("cross matrix", cross_matrix)
 
-                            num_samples_aux = num_samples_aux - 1
+                        # discretizations = discretizations - 1
+                        # print("cross matrix final", cross_matrix)
+                        # cross_amount_array = np.append(cross_amount_array, cross_amount)
+                        # aux_array = cross_array
+                        # non_zero_indices = aux_array != 0
+                        # new_random_values = np.random.rand(non_zero_indices.sum()).round(3)
+                        # aux_array[non_zero_indices] = new_random_values
+                        
+                else:
+                    for e in range(len(distance_array)):
+                        cross_array = np.array([])
+                        cross_amount = 0
+                        cant_cross_amount = 0
+                        cant_cross_amount = False
+                        num_samples_aux = round(num_samples / 6)
+                        for i in range(num_samples_aux):
+                            if aux_array[i][0] != 1:
+                                dist_to_collision = -np.log(1 - aux_array[i][0]) / tt_cross_section
+                            else:
+                                while aux_array[i][0] == 1:
+                                    aux_array[i][0] = round(np.random.rand(), 3)
+                                dist_to_collision = -np.log(1 - aux_array[i][0]) / tt_cross_section
+                            print("dist to collision", dist_to_collision)
+                            # print("dist to collision", dist_to_collision)
+                            if cant_cross_amount != False:
+                                if dist_to_collision < distance_array[e] :
+                                    cant_cross_amount += 1
+                                    cross_array = np.append(
+                                        cross_array, 0.000
+                                    )
+                                    cross_matrix[r][c] = aux_array[i][0]
+                                    print("cross matrix", cross_matrix)
+                                else:
+                                    cross_amount += 1
+                                    cross_array = np.append(cross_array, aux_array[i][0])
+                                    cross_matrix[r][c] = aux_array[i][0]
+                                    print("cross matrix", cross_matrix)
 
-                        else:
-                            cross_amount += 1
-                            cross_array = np.append(cross_array, aux_array[i])
-                            # cant_cross_array = np.append(
-                            #     cant_cross_array, 0.000
-                            # )
+                            else:
+                                if dist_to_collision < distance_array[e]*np.sqrt(2) :
+                                    cant_cross_amount = 1
+                                    cross_amount = 0
+                                    cross_array = np.append(cross_array, 0.000)
+                                    cross_matrix[r][c] = aux_array[i][0]
+                                    print("cross matrix", cross_matrix)
 
-                            num_samples_aux = num_samples_aux - 1
+                                else:
+                                    cross_amount = 1
+                                    cross_array = np.append(cross_array, aux_array[i][0])
+                                    cant_cross_amount = 0
+                                    cross_matrix[r][c] = aux_array[i][0]
+                                    print("cross matrix", cross_matrix)
+                                    
 
-                    else:
-                        if np.any(prob_abs[j, e] > aux_array[i]) or np.any(prob_scat[j, e] > aux_array[i]):
-                            cant_cross_amount = 1
-                            # cant_cross_array = np.append(
-                            #     cant_cross_array, aux_array[0])
-                            cross_amount = 0
-                            cross_array = np.append(cross_array, 0.000)
-                            num_samples_aux = num_samples_aux - 1
+                        print("cross matrix final", cross_matrix)
+                        # cross_amount_array = np.append(cross_amount_array, cross_amount)
+                        # aux_array = cross_array
+                        # non_zero_indices = aux_array != 0
+                        # new_random_values = np.random.rand(non_zero_indices.sum()).round(3)
+                        # aux_array[non_zero_indices] = new_random_values
+                        # print("aux_array", aux_array)
+                        
 
-                        else:
-                            cross_amount = 1
-                            cross_array = np.append(cross_array, aux_array[i])
-                            # cant_cross_array = np.append(
-                            #     cant_cross_array, 0.000
-                            # )
-                            cant_cross_amount = 0
-                            num_samples_aux = num_samples_aux - 1
-
-                discretizations_aux = discretizations_aux - 1
-                aux_array = cross_array
-                non_zero_indices = aux_array != 0
-                new_random_values = np.random.rand(
-                    non_zero_indices.sum()).round(3)
-                aux_array[non_zero_indices] = new_random_values
-                print("discretization:  ", discretizations_aux)
-                print("aux_array", aux_array)
-                print("cross_array:  ", cross_array)
-            cross_matrix = np.vstack((cross_matrix, cross_array))
-        return cross_matrix
+        return cross_amount_array
 
 
 micro_scattering_U235 = 15.04 * 10 ** (-24)
@@ -110,34 +155,41 @@ macro_cs_UO2_absorption = macro_cs_gamma_fuel + macro_cs_fission
 
 
 macro_tt_UO2 = macro_cs_UO2_scattering + macro_cs_UO2_absorption
-prob_scat_UO2 = macro_cs_UO2_scattering/macro_tt_UO2
-prob_abs_UO2 = macro_cs_UO2_absorption/macro_tt_UO2
-print("prob_abs UO2", prob_abs_UO2)
-print("prob_scat UO2", prob_scat_UO2)
 
-print("macro_scattering_Fe", macro_scattering_Fe)
+# print("macro tt cross section", macro_tt_UO2)
+
 
 macro_cs_Fe_absorption = macro_gamma_Fe
 macro_tt_Fe = macro_cs_Fe_absorption + macro_scattering_Fe
-prob_scat_Fe = macro_scattering_Fe/macro_tt_Fe
-pron_abs_Fe = macro_cs_Fe_absorption / macro_tt_Fe
-print("pron_abs_Fe", pron_abs_Fe)
-print("prob_scat_Fe", prob_scat_Fe)
 
-initial_num_discr = int(input('Choose a initial number of discretization:   '))
+print("range", range(3))
+
 initial_neutrons = int(input("Choose initial number of neutrons:    "))
+distance = int(input("Distance:     "))
 
 
-prob_abs = np.matrix([[prob_abs_UO2, prob_abs_UO2], [prob_abs_UO2, prob_abs_UO2],
-                    [prob_abs_UO2, prob_abs_UO2], [prob_abs_UO2, prob_abs_UO2], [prob_abs_UO2, prob_abs_UO2], [prob_abs_UO2, prob_abs_UO2]])
-prob_scat = np.matrix([[prob_scat_UO2, prob_scat_UO2], [prob_scat_UO2, prob_scat_UO2],
-                     [prob_scat_UO2, prob_scat_UO2], [prob_scat_UO2, prob_scat_UO2], [prob_scat_UO2, prob_scat_UO2], [prob_scat_UO2, prob_scat_UO2]])
+distance_array = np.array([])
+for i in range(distance):
+    distance_array = np.append(distance_array, i+1)
 
-aux = One_d_one_material(
-    initial_neutrons, initial_num_discr, prob_abs, prob_scat)
-cross_matrix = aux.n_neutrons_cross(
-    initial_neutrons, initial_num_discr, prob_abs, prob_scat)
-print("matrix that crosses:       ", cross_matrix)
+aux = Two_d_one_material(
+    initial_neutrons, distance_array, macro_tt_UO2)
+cross_amount_array = aux.n_neutrons_cross(
+    initial_neutrons, distance_array, macro_tt_UO2)
+print("amount that crosses:       ", cross_amount_array)
+
+# aux = 1
+
+# cross_df = pd.DataFrame({'Distance': distance_array, 'CrossAmount': cross_amount_array, 'Index': aux})
+
+
+# # Criar um heatmap
+# heatmap_data = cross_df.pivot( index = 'Index', columns='Distance', values='CrossAmount')
+# sns.heatmap(heatmap_data, cmap='viridis', annot=True, fmt='.1f', cbar_kws={'label': 'Cross Amount'})
+# plt.xlabel('Distance')
+# plt.ylabel('Sample')
+# plt.title('Neutron Cross Amount Heatmap')
+# plt.show()
 
 # strange value https://www.dgtresearch.com/diffusion-coefficients/
 # https://www.nuclear-power.com/nuclear-power/reactor-physics/neutron-diffusion-theory/neutron-current-density/
