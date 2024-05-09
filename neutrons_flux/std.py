629590804fb8e11829bd5d3a5906f056cda908cb
carol_batista <carol.batista.alvares@gmail.com>
2024-05-09 18:19:48 -0300

    new two dimen branch

--- neutrons_flux/std.py
+++ neutrons_flux/std.py
@@ -3,92 +3,102 @@ import pandas as pd
 import numpy as np
 
 
-row1_python50 = np.array([19268, 43654, 48772, 43654, 19268])
-row2_python50 = np.array([40945, 90022, 98154, 90022, 40945])
-row3_python50 = np.array([43354, 92736, 173415, 92736, 43354])
-row4_python50 = np.array([40945, 90022, 98154, 90022, 40945])
-row5_python50 = np.array([19268, 43654, 48772, 43654, 19268])
-total_python50 = np.array(
+row1_python = np.array([ 10394,  20930,  28445,  20711,  10337])
+row2_python = np.array( [ 20829,  46065,  72324,  46096,  21339])
+row3_python = np.array( [ 28314,  72135, 171527 , 72222,  28880])
+row4_python = np.array([ 21010,  46415,  73140,  46308,  21254])
+row5_python = np.array([ 10347 , 21232,  28629 , 21249 , 10474])
+total_python = np.array(
     [
-        19268,
-        43654,
-        48772,
-        43654,
-        19268,
-        40945,
-        90022,
-        98154,
-        90022,
-        40945,
-        43354,
-        92736,
-        173415,
-        92736,
-        43354,
-        40945,
-        90022,
-        98154,
-        90022,
-        40945,
-        19268,
-        43654,
-        48772,
-        43654,
-        19268,
+   10394,  20930,  28445,  20711,  10337,  20829,  46065,  72324,  46096,  21339, 28314,  72135, 171527 , 72222,  28880,  21010,  46415,  73140,  46308,  21254, 10347 , 21232,  28629 , 21249 , 10474
     ]
 )
 
 
-df_py_50 = pd.DataFrame(
+df_py = pd.DataFrame(
     {
-        "Coluna1": row1_python50,
-        "Coluna2": row2_python50,
-        "Coluna3": row3_python50,
-        "Coluna4": row4_python50,
-        "Coluna5": row5_python50,
+        "Coluna1": row1_python,
+        "Coluna2": row2_python,
+        "Coluna3": row3_python,
+        "Coluna4": row4_python,
+        "Coluna5": row5_python,
     }
 )
 
 fig = plt.figure()
-plt.imshow(df_py_50, origin="lower", cmap="viridis", extent=(-50, 50, -50, 50))
-plt.colorbar(label="Fluxo de Nêutrons Python lado 50")
+plt.imshow(df_py, origin="lower", cmap="viridis", extent=(-50, 50, -50, 50))
+plt.colorbar(label="Fluxo de Nêutrons Python lado 1")
 plt.title("Fluxo de Nêutrons Python lado 50")
 plt.xlabel("X [cm]")
 plt.ylabel("Y [cm]")
 plt.show()
 
-row1_openmc50 = np.array([3900, 3978, 3925, 3956, 3849])
-row2_openmc50 = np.array([3946, 4116, 4103, 3976, 3928])
-row3_openmc50 = np.array([4013, 4137, 4141, 3985, 3938])
-row4_openmc50 = np.array([4078, 4222, 4079, 3909, 3974])
-row5_openmc50 = np.array([3879, 4006, 4082, 3980, 3900])
+
+
+
+row1_openmc50 = np.array([523 	,
+2169 	,
+3572 	,
+2185 	,
+526 	
+])
+row2_openmc50 = np.array([2198 	,
+12661 	,
+24161 	,
+12596 	,
+2174 	
+
+])
+row3_openmc50 = np.array([
+    3606 	,
+24215 	,
+49360 	,
+24187 	,
+3568 	])
+row4_openmc50 = np.array([2171 	,
+12650 	,
+24268 	,
+12612 	,
+2196 	
+
+])
+row5_openmc50 = np.array([ 
+                          513 	,
+2158 	,
+3632 	,
+2171 	,
+527 	
+
+])
 total_openmc50 = np.array(
     [
-        3900,
-        3978,
-        3925,
-        3956,
-        3849,
-        3946,
-        4116,
-        4103,
-        3976,
-        3928,
-        4013,
-        4137,
-        4141,
-        3985,
-        3938,
-        4078,
-        4222,
-        4079,
-        3909,
-        3974,
-        3879,
-        4006,
-        4082,
-        3980,
-        3900,
+        523 	,
+2169 	,
+3572 	,
+2185 	,
+526 	,
+2198 	,
+12661 	,
+24161 	,
+12596 	,
+2174 	,
+3606 	,
+24215 	,
+49360 	,
+24187 	,
+3568 	,
+2171 	,
+12650 	,
+24268 	,
+12612 	,
+2196 	,
+513 	,
+2158 	,
+3632 	,
+2171 	,
+527 	,
+
+
     ]
 )
 
@@ -112,7 +122,7 @@ plt.show()
 
 np_array_10 = np.array([])
 for i in range(25):
-    aux = (abs(total_python50[i] - total_openmc50[i])) * 100 / total_python50[i]
+    aux = (abs(total_python[i] - total_openmc50[i])) * 100 / total_python[i]
     np_array_10 = np.append(np_array_10, aux)
     # print("np_array1", np_array_50)
 sum_1 = np.sum(np_array_10)
@@ -120,22 +130,4 @@ sum_1 = np.sum(np_array_10)
 reshapes = np_array_10.reshape((5, 5))
 
 
-for i in range(1):
-    df_10 = pd.DataFrame(
-        {
-            "Coluna1": reshapes[i],
-            "Coluna2": reshapes[i + 1],
-            "Coluna3": reshapes[i + 2],
-            "Coluna4": reshapes[i + 3],
-            "Coluna5": reshapes[i + 4],
-        }
-    )
-fig = plt.figure()
-plt.imshow(df_10, origin="lower", cmap="viridis", extent=(-5, 5, -5, 5))
-plt.colorbar(label="Fluxo de Nêutrons python 1")
-plt.title("STD")
-plt.xlabel("X [cm]")
-plt.ylabel("Y [cm]")
-plt.show()
-
-print("para lado 10 do python e lado 10 openmc:  ", np.std(np_array_10))
+print("std:  ", np.std(np_array_10))
