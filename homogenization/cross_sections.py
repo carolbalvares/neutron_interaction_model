2a8182146e8089383188f148b9e12e63a53084ca
carol_batista <carol.batista.alvares@gmail.com>
2024-07-03 11:51:41 -0300

    one dimension finite diferences working (1d_df)

--- homogenization/cross_sections.py
+++ homogenization/cross_sections.py
@@ -35,9 +35,11 @@
 #U-Fe(N,F) = No Data Found
 #U-Fe(N,G) = 2.6213868
 #U-Fe(N,NON) = No Data Found
-
-from parameters import *
 import sys
+
+sys.path.append("../")
+from parameters import *
+
 
 ##Gamma
 macro_gamma_Fe = 2.813
@@ -59,10 +61,19 @@ micro_cs_gamma = (((macro_gamma_U235 + macro_gamma_U238 + macro_gamma_O)*(tt_vol
 micro_fission_U235 = 590.64465
 micro_fission_U238 = 1.86321*10**(-5)
 
-macro_fission_U235 = micro_fission_U235 * n_U235
-macro_fission_U238 = micro_fission_U238 * n_U238
+macro_fission_U235 = micro_fission_U235 * n_U235* 10 ** (-24)
+macro_fission_U238 = micro_fission_U238 * n_U238* 10 ** (-24)
 
 macro_cs_fission = (macro_fission_U235 + macro_fission_U238) * tt_vol_UO2 / tt_act_core_vol
+# print("macro_fission_U235",macro_fission_U235)
+# print("macro_fission_U238",macro_fission_U238)
+
+# print("",)
+# print("",)
+
+
+Na = 6.02214076 * 10**(23)
+
 micro_cs_fission = macro_cs_fission / n_UO2
 
 ##Scattering
@@ -82,8 +93,21 @@ macro_cs_scattering = (((macro_scattering_U235 + macro_scattering_U238 + macro_s
                        (macro_scattering_H2O * tt_vol_H2O) + (macro_scattering_Fe * tt_vol_Fe))/tt_act_core_vol
 micro_cs_scattering = (((macro_scattering_U235 + macro_scattering_U238 + macro_scattering_O)*(tt_vol_UO2)) +
                        (macro_scattering_H2O * tt_vol_H2O) + (macro_scattering_Fe * tt_vol_Fe))/(tt_act_core_vol * (n_UO2 + n_H2O + n_Fe))
-
+# print("micro_cs_scattering", micro_cs_scattering)
 ##Absorption
 micro_abs_H2O = 0.66 * 10**(-24)
 macro_cs_absorption = macro_cs_gamma + macro_cs_fission
-micro_cs_absorption = macro_cs_absorption/(n_UO2 + n_H2O + n_Fe)
+micro_cs_absorption = (macro_cs_absorption/(n_UO2 + n_H2O + n_Fe))
+# print("macro_cs_gamma", macro_cs_gamma)
+# print("macro_cs_fission", macro_cs_fission)
+# print("micro_cs_absorption", micro_cs_absorption)
+print("macro_cs_absorption", macro_cs_absorption)
+# macro_cs_gamma 0.8205873192283825
+# macro_cs_fission 2.0560876547678343e+23
+# micro_cs_absorption 1.450667024580102e-24
+# macro_cs_absorption macro_cs_absorption 1.0261960847051659
+# print("n_UO2",n_UO2)
+# print("n_H2O",n_H2O)
+# print("n_Fe",n_Fe)
+micro_cs_absorptiona = (macro_cs_absorption * tt_vol_UO2) / (tt_act_core_vol * (n_UO2 + n_H2O + n_Fe))
+# print("oii",micro_cs_absorptiona )
